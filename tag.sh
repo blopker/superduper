b50ada5e6bfe03d106b3f95649f45b337a762f50
Bo Lopker <lopkerk@gmail.com>
2023-01-13 14:08:41 -0800

    URL encode

--- tag.sh
+++ tag.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
-VER=$(grep 'version:' pubspec.yaml | cut -d " " -f 2)
-git commit -am "v$VER"
-git tag "v$VER"
+VER=v$(grep 'version:' pubspec.yaml | cut -d " " -f 2)
+git commit -am "$VER"
+git tag "$VER"
 git push --all
 git push --tags
-echo "Release notes: https://github.com/octo-org/octo-repo/releases/new?tag=$VER&title=Release%20$VER"
+
+# URL encode
+CLEANVER=$(curl -s -w '%{url_effective}\n' -G / --data-urlencode "=$VER" | cut -c 3-)
+echo "Release notes: https://github.com/blopker/superduper/releases/new?tag=$CLEANVER&title=Release%20$CLEANVER"
