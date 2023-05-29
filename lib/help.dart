9cf3da5da7c33be6caf5ea053da3268484c061aa
Bo Lopker <lopkerk@gmail.com>
2023-05-29 11:31:37 -0700

    Move help to scaffold and appbar

--- lib/help.dart
+++ lib/help.dart
@@ -51,60 +51,52 @@ class HelpWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        height: MediaQuery.of(context).size.height,
-        color: Colors.black87,
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            const SizedBox(
-              height: 40,
-            ),
-            Row(
+    return Padding(
+      padding: const EdgeInsets.only(top: 40),
+      child: Scaffold(
+        appBar: AppBar(
+            backgroundColor: Colors.black87,
+            title: const Text('Help'),
+            leading: IconButton(
+              icon: const Icon(Icons.arrow_back),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            )),
+        body: Container(
+            height: MediaQuery.of(context).size.height,
+            color: Colors.black87,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
               children: [
-                IconButton(
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                    icon: const Icon(
-                      Icons.arrow_back,
-                      size: 35,
-                    )),
-                Center(
-                  child: Padding(
-                    padding: const EdgeInsets.only(top: 3.0),
-                    child: Text('Help',
-                        style: Theme.of(context).textTheme.titleLarge),
-                  ),
-                )
+                Expanded(
+                  child: ListView(shrinkWrap: true, children: [
+                    Padding(
+                      padding: const EdgeInsets.only(
+                          left: 20, right: 20, bottom: 20),
+                      child: MarkdownBody(
+                        styleSheet: MarkdownStyleSheet(
+                            h1Padding:
+                                const EdgeInsets.only(top: 10, bottom: 10),
+                            h2Padding:
+                                const EdgeInsets.only(top: 10, bottom: 10),
+                            h3Padding:
+                                const EdgeInsets.only(top: 10, bottom: 10)),
+                        selectable: true,
+                        onTapLink: (text, href, title) {
+                          if (href != null) {
+                            launchUrl(Uri.parse(href),
+                                mode: LaunchMode.externalApplication);
+                          }
+                        },
+                        data: helpText,
+                      ),
+                    )
+                  ]),
+                ),
               ],
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            Expanded(
-              child: ListView(shrinkWrap: true, children: [
-                Padding(
-                  padding:
-                      const EdgeInsets.only(left: 20, right: 20, bottom: 20),
-                  child: MarkdownBody(
-                    styleSheet: MarkdownStyleSheet(
-                        h1Padding: const EdgeInsets.only(top: 10, bottom: 10),
-                        h2Padding: const EdgeInsets.only(top: 10, bottom: 10),
-                        h3Padding: const EdgeInsets.only(top: 10, bottom: 10)),
-                    selectable: true,
-                    onTapLink: (text, href, title) {
-                      if (href != null) {
-                        launchUrl(Uri.parse(href),
-                            mode: LaunchMode.externalApplication);
-                      }
-                    },
-                    data: helpText,
-                  ),
-                )
-              ]),
-            ),
-          ],
-        ));
+            )),
+      ),
+    );
   }
 }
